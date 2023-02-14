9761b451e0bb9ccdf2ffb4bc00d5d54701e1e6c1
LaurenChapman97 <lauren.c.c@hotmail.com>
2023-02-14 13:58:30 -0300

    setting up lambda function for receiving event

--- Yellow/transformation_function.py
+++ Yellow/transformation_function.py
@@ -21,3 +21,19 @@ print(response['Body'])
 
 with open('retrieved.txt', 'wb') as f:
     f.write(response['Body'].read())
+
+
+"Setting up a lambda function to recieve s3 put object event (triggered when a file is added to the bucket)"
+
+def lambda_handler(event, context):
+
+    Args:
+        event:
+            a valid S3 PutObject event -
+            see https://docs.aws.amazon.com/AmazonS3/latest/userguide/notification-content-structure.html
+        context:
+            a valid AWS lambda Python context object - see
+            https://docs.aws.amazon.com/lambda/latest/dg/python-context.html
+
+            https://docs.aws.amazon.com/eventbridge/latest/userguide/eb-get-started.html
+            
