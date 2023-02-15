0802cb58dd5ffcb0a0ad62dc5dab7c03ca32cfeb
LaurenChapman97 <lauren.c.c@hotmail.com>
2023-02-15 12:07:25 -0300

    creating zip file

--- transform-lambda/src/transformation_main.py
+++ transform-lambda/src/transformation_main.py
@@ -1,4 +1,5 @@
 # import packages
+import zipfile
 import boto3
 import pandas as pd
 # python logging module
@@ -50,25 +51,15 @@ obj_list = s3.list_objects_v2(Bucket=bucket_two)
 print(obj_list['Contents'])
 
 
-"Setting up a lambda function to recieve s3 put object event (triggered when a file is added to the bucket)"
+# Create a new zip file
+with zipfile.ZipFile('lambda-deployment.zip', 'w') as zip:
+    # Add files to the zip file
+    zip.write('lambda-handler.py')
 
+# create S3 bucket for lambda handler
+bucket_three = 'sqhellsangels-lambda-bucket'
+s3.create_bucket(Bucket=bucket_three)
 
-def lambda_handler(event, context):
-    pass
-
-
-# Args:
-#     event:
-#         a valid S3 PutObject event -
-#         see https://docs.aws.amazon.com/AmazonS3/latest/userguide/notification-content-structure.html
-#     context:
-#         a valid AWS lambda Python context object - see
-#         https://docs.aws.amazon.com/lambda/latest/dg/python-context.html
-
-#         https://docs.aws.amazon.com/eventbridge/latest/userguide/eb-get-started.html
-
-
-# iam = boto3.client('iam', region_name='us-east-1')
-# policy = '{"Version": "2012-10-17","Statement": [{ "Effect": "Allow", "Principal": {"Service": "lambda.amazonaws.com"},"Action": "sts:AssumeRole"}]}
-# response = iam.create_role( RoleName=role_name, AssumeRolePolicyDocument=lambda_role_document)
-# print(iam.list_roles())
+# List the objects in 'processed' s3 bucket
+lambda_list = s3.list_objects_v2(Bucket=bucket_three)
+print(lambda_list['Contents'])
