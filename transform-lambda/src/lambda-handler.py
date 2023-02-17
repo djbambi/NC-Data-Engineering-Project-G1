b9c6c753056a861b82fb0347e2a9bff01378a652
LaurenChapman97 <lauren.c.c@hotmail.com>
2023-02-17 11:07:00 -0300

    reset

--- transform-lambda/src/lambda-handler.py
+++ transform-lambda/src/lambda-handler.py
@@ -1,49 +1,19 @@
-import logging
 import boto3
-from botocore.exceptions import ClientError
 import pandas as pd
 
 # create clients
 s3 = boto3.client('s3')
-
-
 def lambda_handler(event, context):
-    # bucket name variables
-    bucket_one = 'sqhellsangels-ingestion-bucket'
-    bucket_two = 'sqhellsangels-processed-bucket'
-
-    # GET FILE
-    response = s3.get_object(Bucket=bucket_one, Key='test.csv')
-    with open('/tmp/retrieved.csv', 'wb') as f:
-        f.write(response['Body'].read())
-
-    # TRANSFORM FILE
-    df = pd.read_csv('/tmp/retrieved.csv')
+    # Get CSV file from ingestion bucket
+    bucket_one = 'sqhellsangels-ingestion-buckets'
+    key = 'test.csv'
+    response = s3.get_object(Bucket=bucket_one, Key=key)
+    # read CSV file into a Pandas DataFrame
+    df = pd.read_csv(response['Body'])
     # convert DataFrame to Parquet format and save to file
-    df.to_parquet('/tmp/transformed-data.parquet')
-
-   # response = s3.get_object(Bucket=bucket_one, Key='test.csv')
-    # object_content = response['Body'].read()
-   # print(object_content)
-
-    # df = pd(object_content)
-    # df.to_parquet('transformed-data.parquet')
-
-    with open('/tmp/transformed-data.parquet', 'rb') as f:
-        s3.put_object(Bucket=bucket_two,
-                      Key='transformed-data.parquet', Body=f)
-
-
-'''lAMBDA JOBS
-.GET CSV FILE FROM INGESTION BUCKET
-.READ CSV FILE, TURN INTO DATA FRAME WITH PANDAS, TRANSFORM TO PARQUET
-.UPLOAD CONVERTED DATA TO TRANSFORMED S3 BUCKET
-.
-
-'''
-lambda_handler(event="hello", context="hello")
-
-# iam = boto3.client('iam', region_name='us-east-1')
-# policy = '{"Version": "2012-10-17","Statement": [{ "Effect": "Allow", "Principal": {"Service": "lambda.amazonaws.com"},"Action": "sts:AssumeRole"}]}
-# response = iam.create_role( RoleName=role_name, AssumeRolePolicyDocument=lambda_role_document)
-# print(iam.list_roles())
+    transformed_data = df.to_parquet()
+    # Upload object to bucket
+    bucket_two = 'sqhellsangels-processed-bucket'
+    key = 'transformed-data.parquet'
+    s3.put_object(Bucket=bucket_two, Key=key, Body=transformed_data)
+    return 'Data transformed and uploaded to S3'
--- transform-lambda/src/script.py
+++ transform-lambda/src/script.py
@@ -1,4 +1,3 @@
-# import packages
 import zipfile
 import boto3
 import json
@@ -7,14 +6,7 @@ import pandas as pd
 # create clients
 s3 = boto3.client('s3')
 
-
-bucket_three = 'sqhellsangels-lambda-bucket'
-
 # Create a new zip file
 with zipfile.ZipFile('lambda-deployment.zip', 'w') as zip:
     # Add files to the zip file
     zip.write('lambda-handler.py')
-
-# Add ZIP to s3 bucket
-s3.upload_file('./lambda-deployment.zip',
-               bucket_three, 'lambda-deployment.zip')
