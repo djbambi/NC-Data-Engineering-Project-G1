f08c687b504c72b47414fc6c465babeb37707b4c
Abdurrahman Naveed <ar14@outlook.com>
2023-02-20 16:46:01 +0000

    Added doc strings

--- ingestion-lambda/src/convert_to_csv.py
+++ ingestion-lambda/src/convert_to_csv.py
@@ -1,7 +1,7 @@
 import csv
 import os
 
-
+#This function takes a list of dictionaries of table data, which will be outputted by the ingest and the get_updated_data functions. it creates a new directory and saves each table as a seperate csv file inside that folder
 def convert_to_csv(tables_list):
     os.makedirs("src/csv_files")
     for table_dict in tables_list:
@@ -10,6 +10,7 @@ def convert_to_csv(tables_list):
                 writing_func(table_dict[key], key)
 
 
+#Helper function for convert_to_csv: writes any list provided to a file with the provided name into a folder called csv_files
 def writing_func(list, table_name):
     with open(f"src/csv_files/{table_name}.csv", "w", encoding="utf-8") as file:
         writer = csv.writer(file)
