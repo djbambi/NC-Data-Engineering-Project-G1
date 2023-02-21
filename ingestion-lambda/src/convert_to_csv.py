316ae9b9376ebc2327a1490db4f0cfbe66921a53
Abdurrahman Naveed <ar14@outlook.com>
2023-02-21 13:32:57 +0000

    Test functionality for converting to csv

--- ingestion-lambda/src/convert_to_csv.py
+++ ingestion-lambda/src/convert_to_csv.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import json
 
 #This function takes a list of dictionaries of table data, which will be outputted by the ingest and the get_updated_data functions. it creates a new directory and saves each table as a seperate csv file inside that folder
 def convert_to_csv(tables_list):
@@ -8,6 +9,16 @@ def convert_to_csv(tables_list):
         for key in table_dict.keys():
             if table_dict[key]:
                 writing_func(table_dict[key], key)
+
+    csv_list = []
+    for row in tables_list[4]["payment"]:
+        my_array = []
+        for element in row:
+            my_array.append(json.dumps(element, default=str))
+        csv_list.append(",".join(my_array))
+    csv_string = "\n".join(csv_list)
+    print(csv_string)
+
 
 
 #Helper function for convert_to_csv: writes any list provided to a file with the provided name into a folder called csv_files
--- ingestion-lambda/test/test_convert_to_csv.py
+++ ingestion-lambda/test/test_convert_to_csv.py
@@ -5,47 +5,54 @@ import shutil
 import csv
 
 
-def test_writes_a_csv_file():
+# def test_writes_a_csv_file():
+#     if os.path.exists("./src/csv_files"):
+#         shutil.rmtree("src/csv_files")
+
+#     convert_to_csv(mock_data)
+
+#     assert os.path.exists("./src/csv_files/payment.csv") == True
+
+
+# def test_writes_csv_files_():
+#     if os.path.exists("./src/csv_files"):
+#         shutil.rmtree("src/csv_files")
+
+#     convert_to_csv(mock_data)
+
+#     assert os.path.exists("./src/csv_files/payment.csv") == True
+#     assert os.path.exists("./src/csv_files/transaction.csv") == True
+#     assert os.path.exists("./src/csv_files/sales_order.csv") == True
+
+
+# def test_doesnt_save_empty_files():
+#     if os.path.exists("./src/csv_files"):
+#         shutil.rmtree("src/csv_files")
+
+#     assert os.path.exists("./src/csv_files/design.csv") == False
+#     assert os.path.exists("./src/csv_files/staff.csv") == False
+
+
+# def test_column_names_exist():
+#     if os.path.exists("./src/csv_files"):
+#         shutil.rmtree("src/csv_files")
+
+#     convert_to_csv(mock_data2)
+
+#     with open("./src/csv_files/payment.csv", "r") as file:
+#         if os.path.exists("./src/csv_files"):
+#             shutil.rmtree("src/csv_files")
+
+#         convert_to_csv(mock_data2)
+
+#         reader = csv.reader(file)
+#         row1 = next(reader)
+#         assert "payment_id" in row1
+#         assert "last_updated" in row1
+
+def test_converts_json():
     if os.path.exists("./src/csv_files"):
         shutil.rmtree("src/csv_files")
-
-    convert_to_csv(mock_data)
-
-    assert os.path.exists("./src/csv_files/payment.csv") == True
-
-
-def test_writes_csv_files_():
-    if os.path.exists("./src/csv_files"):
-        shutil.rmtree("src/csv_files")
-
-    convert_to_csv(mock_data)
-
-    assert os.path.exists("./src/csv_files/payment.csv") == True
-    assert os.path.exists("./src/csv_files/transaction.csv") == True
-    assert os.path.exists("./src/csv_files/sales_order.csv") == True
-
-
-def test_doesnt_save_empty_files():
-    if os.path.exists("./src/csv_files"):
-        shutil.rmtree("src/csv_files")
-
-    assert os.path.exists("./src/csv_files/design.csv") == False
-    assert os.path.exists("./src/csv_files/staff.csv") == False
-
-
-def test_column_names_exist():
-    if os.path.exists("./src/csv_files"):
-        shutil.rmtree("src/csv_files")
-
+        
     convert_to_csv(mock_data2)
-
-    with open("./src/csv_files/payment.csv", "r") as file:
-        if os.path.exists("./src/csv_files"):
-            shutil.rmtree("src/csv_files")
-
-        convert_to_csv(mock_data2)
-
-        reader = csv.reader(file)
-        row1 = next(reader)
-        assert "payment_id" in row1
-        assert "last_updated" in row1
+    assert 1 == 2
