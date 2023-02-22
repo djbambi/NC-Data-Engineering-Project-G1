de5f35c758f0f883b2f5b31b1b757b3b605bc25b
Abdurrahman Naveed <ar14@outlook.com>
2023-02-22 16:31:00 +0000

    Completed retrieving, uploading, and creating a timestamp

--- ingestion-lambda/src/timestamp.py
+++ ingestion-lambda/src/timestamp.py
@@ -1,4 +1,28 @@
 from datetime import datetime
+import boto3
+from src.bucket_names import ingestion_bucket
 
+bucket = ingestion_bucket
+timestamp_key = 'timestamp/latest-timestamp.txt'
+client = boto3.client("s3")
+
+
+"""Creates a timestamp when the lambda is run, to be used for the next time the lambda is run to query the SQL tote database, and to save the csv files into a folder with the timestamp as its name"""
 def create_timestamp():
-    return datetime.now().strftime("%Y-%m-%d %H:%M")
+    current_time = datetime.now()
+
+    timestamp = current_time.strftime("%Y-%m-%d %H:%M")
+
+    return timestamp
+ 
+
+"""Retrives latest timestamp from the timesstamp folder in the s3 ingestion bucket"""
+def retrieve_timestamp():
+    data = client.get_object(Bucket=bucket, Key=timestamp_key)
+    contents = data['Body'].read().decode()
+    return contents
+
+
+"""Uploads new timestamp to s3 ingestion bucket"""
+def upload_timestamp(timestamp):
+    client.put_object(Body=timestamp, Bucket=bucket, Key=timestamp_key)
--- ingestion-lambda/test/test_timestamp.py
+++ ingestion-lambda/test/test_timestamp.py
@@ -1,24 +1,40 @@
-from src.timestamp import create_timestamp
+from src.timestamp import create_timestamp, retrieve_timestamp, upload_timestamp
 from datetime import datetime
 import unittest
 from unittest.mock import patch, Mock
+import json
+from botocore.response import StreamingBody
+import io
 
-# def test_returns_date_and_time_as_a_string():
-    # time_mock = Mock()
-    # return_value="2023-02-22 11:43:42.214110"
-    # with patch(dt=DEFAULT) as mock_i:
-    #     mock.dt.now.return_value = datetime(2023, 2, 22, 11, 43)
-    
-    # with patch("src.timestamp.datetime.now") as m:
-    #     m.return_value = Mock(return_value=datetime.datetime(2023, 2, 22, 11, 43))
-    #     assert create_timestamp() == "H"
-    # assert create_timestamp() == datetime.now().strftime("%Y-%m-%d %H:%M")
+body_json = '2023-02-22 11:51'
+
+body_encoded = json.dumps(body_json).encode('utf-8')
+
+body = StreamingBody(
+    io.BytesIO(body_encoded),
+    len(body_encoded)
+)
+
+mocked_response = {
+    'Body': body
+}
 
 
 class Test(unittest.TestCase):
 
-    @patch('src.timestamp.datetime')
-    def test_returns_date_and_time_as_a_string(self, datetime_mock):
-        datetime_mock.now = Mock(return_value=datetime(2023, 2, 22, 11, 43))
+    # @patch('src.timestamp.datetime')
+    # def test_create_timestamp_returns_date_and_time_as_a_string(self, datetime_mock):
+    #     datetime_mock.now = Mock(return_value=datetime(2023, 2, 22, 11, 43))
         
-        assert create_timestamp() == "2023-02-22 11:43"
+    #     assert create_timestamp() == "2023-02-22 11:43"
+
+
+    @patch('src.timestamp.client')
+    def test_retrieve_timestamp_reads_latest_timestamp_from_s3(self, boto3_mock):
+        boto3_mock.get_object = Mock(return_value=mocked_response)
+        
+        assert retrieve_timestamp()[1:17] == "2023-02-22 11:51"
+
+
+def test_upload_timestamp_uploads_to_s3():
+    assert upload_timestamp("2023-02-22 12:51") == None
