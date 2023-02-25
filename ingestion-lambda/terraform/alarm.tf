feeafab3e1b5e69578333314c7c206f88b3133f6
Abdurrahman Naveed <ar14@outlook.com>
2023-02-25 12:11:11 +0000

    Added SNS topic and alarm to an email in the vars file

---
+++ ingestion-lambda/terraform/alarm.tf
@@ -0,0 +1,35 @@
+resource "aws_cloudwatch_log_metric_filter" "ingestion_lambda_error" {
+    name = "error-filter"
+    pattern = "Error"
+    log_group_name = "/aws/lambda/${var.ingestion_lambda_name}"
+
+    metric_transformation {
+      name = "errors"
+      namespace = "ingestion_lambda"
+      value = "1"
+    }
+}
+
+resource "aws_cloudwatch_metric_alarm" "alert_errors" {
+    alarm_name = "error-alarm"
+    comparison_operator = "GreaterThanOrEqualToThreshold"
+    evaluation_periods = "1"
+    metric_name = "errors"
+    namespace = "ingestion_lambda"
+    period = "60"
+    statistic = "Sum"
+    threshold = "1"
+    alarm_description = "Error detected in ingestion_lambda."
+    insufficient_data_actions = []
+    alarm_actions = [ aws_sns_topic.error_notifications_topic.arn ]
+}
+
+resource "aws_sns_topic" "error_notifications_topic" {
+  name = "error_notifications_topic"
+}
+
+resource "aws_sns_topic_subscription" "error_notifications_email" {
+  topic_arn = aws_sns_topic.error_notifications_topic.arn
+  protocol  = "email"
+  endpoint  = "${var.notifications_email}"
+}
