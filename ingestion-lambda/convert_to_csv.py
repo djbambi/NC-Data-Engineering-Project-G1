6c1bed9d0b1e3cf5696ff16a766b6b7e018cb9e5
Abdurrahman Naveed <ar14@outlook.com>
2023-02-27 13:17:44 +0000

    Complete functionality for upload_to_s3 function

--- ingestion-lambda/convert_to_csv.py
+++ ingestion-lambda/convert_to_csv.py
@@ -1,40 +1,38 @@
-import csv
-from ingest_to_s3 import transfer_to_s3
-import os
 import json
 import boto3
 from bucket_names import ingestion_bucket
-dirname = os.path.dirname(__file__)
-path_to_csv_files = "csv_files"
+import re
 
 client = boto3.client("s3")
 
-#This function takes a list of dictionaries of table data, which will be outputted by the ingest and the get_updated_data functions. it creates a new directory and saves each table as a seperate csv file inside that folder
+
+"""This function takes a list of dictionaries of table data, which will be outputted by the ingest and the get_updated_data functions. it creates a new directory and saves each table as a seperate csv file inside that folder"""
 def convert_to_csv(tables_list, timestamp):
     for table_dict in tables_list:
         for key in table_dict.keys():
-            # print(table_dict[key], key)
             if table_dict[key]:
-                writing_func(table_dict[key], key, timestamp)
+                upload_to_s3(table_dict[key], key, timestamp)
 
 
-#Helper function for convert_to_csv: writes any list provided to a file with the provided name into a folder called csv_files, the output should go to ingest_to_S3 function to be uploaded on the Ingest S3 bucket
+"""Helper function to take each table, convert it to a string, and write it to the correct path and file name in the s3 ingestion bucket"""
+def upload_to_s3(table_rows_list, table_name, timestamp):
 
-# def writing_func(list, table_name, timestamp):    
-#     with open(f"{dirname}/{path_to_csv_files}/{table_name}.csv", "w+", encoding="utf-8") as file:
-#         writer = csv.writer(file)
-#         for row in list:
-#             writer.writerow(row)
+    bucket_key = f"{timestamp}/{table_name}.csv"
+    if "full" in table_name:
+        name_search = re.search("full_(.*)_table", table_name)
+        extracted_name = name_search.group(1)
+        bucket_key = f"{extracted_name}/{table_name}.csv"
 
-#     transfer_to_s3(table_name, timestamp)
-
-def writing_func(table_rows_list, table_name, timestamp):
     csv_list = []
+
     for row in table_rows_list:
         my_array = []
+
         for element in row:
             my_array.append(json.dumps(element, default=str))
+
         csv_list.append(",".join(my_array))
+
     csv_string = "\n".join(csv_list)
-    boto3.put_object(Body=csv_string, Bucket=ingestion_bucket, key=f"{timestamp}/{table_name}.csv")
-    print(csv_string)
+
+    client.put_object(Body=csv_string, Bucket=ingestion_bucket, key=bucket_key)
