4f30e678bad43082dec8f2fd11f04bb24212e12f
Abdurrahman Naveed <ar14@outlook.com>
2023-02-27 14:39:53 +0300

    Refactor timestamp functions

--- ingestion-lambda/timestamp.py
+++ ingestion-lambda/timestamp.py
@@ -2,8 +2,7 @@ from datetime import datetime
 import boto3
 from bucket_names import ingestion_bucket
 
-bucket = ingestion_bucket
-timestamp_key = 'timestamp/latest-timestamp.txt'
+timestamp_key = 'timestamp/latest_timestamp.txt'
 client = boto3.client("s3")
 
 
@@ -12,36 +11,21 @@ def create_timestamp():
     current_time = datetime.now()
     timestamp = current_time.strftime("%Y-%m-%d %H:%M")
 
-    with open(f"/tmp/latest_timestamp.txt", "w+") as file:
-        file.write(timestamp)
-            
-    client = boto3.client('s3')
-    client.upload_file(f'/tmp/latest_timestamp.txt', ingestion_bucket,f'timestamp/latest_timestamp.txt')
-
-    return timestamp
- 
-
-# """Retrives latest timestamp from the timesstamp folder in the s3 ingestion bucket"""
-# def retrieve_timestamp():
-#     data = client.get_object(Bucket=bucket, Key=timestamp_key)
-#     contents = data['Body'].read().decode()
-#     return contents
+    return timestamp 
 
 
-# """Uploads new timestamp to s3 ingestion bucket"""
-# def upload_timestamp(timestamp):
-#     client.put_object(Body=timestamp, Bucket=bucket, Key=timestamp_key)
+"""Uploads new timestamp to s3 ingestion bucket"""
+def upload_timestamp(timestamp):
+    client.put_object(Body=timestamp, Bucket=ingestion_bucket, Key=timestamp_key)
 
 
-##When Abdulrrah comes back we can refactor the top two functions instead of the this bottom one
+"""Retrives latest timestamp from the timesstamp folder in the s3 ingestion bucket"""
 def retrieve_timestamp():
-    client = boto3.client('s3')
     
     result = client.list_objects(Bucket=ingestion_bucket, Prefix="timestamp/")
-    exists=False
 
     if 'Contents' in result:               
-        data = client.get_object(Bucket= ingestion_bucket, Key='timestamp/latest_timestamp.txt')
+        data = client.get_object(Bucket= ingestion_bucket, Key=timestamp_key)
         latest_timestamp = data["Body"].read().decode()
         return latest_timestamp
         
